02acf568477e78c47b4984c019d578063d7c81f4
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-23 16:07:35 +0900

    move inside

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { TestComponentProps } from "./TestComponent.types";
 
 import "./TestComponent.scss";
+
 import * as util from '../agentscript/utils.js'
 import TwoDraw from '../agentscript/TwoDraw.js'
 import Animator from '../agentscript/Animator.js'
@@ -10,30 +11,31 @@ import Color from '../agentscript/Color.js'
 import ColorMap from '../agentscript/ColorMap.js'
 import Model from '../models/AntsModel'
 
-// Define colors and colormaps
-const nestColor = Color.typedColor('yellow')
-const foodColor = Color.typedColor('blue')
-const nestColorMap = ColorMap.gradientColorMap(20, [
-  'black',
-  nestColor,
-])
-const foodColorMap = ColorMap.gradientColorMap(20, [
-  'black',
-  foodColor,
-])
+const TestComponent: React.FC<TestComponentProps> = ({ heading, content }) => {
+  // Define colors and colormaps
+  const nestColor = Color.typedColor('yellow')
+  const foodColor = Color.typedColor('blue')
+  const nestColorMap = ColorMap.gradientColorMap(20, [
+    'black',
+    nestColor,
+  ])
+  const foodColorMap = ColorMap.gradientColorMap(20, [
+    'black',
+    foodColor,
+  ])
 
-const drawOptions = {
-  patchesColor: p => {
-    if (p.isNest) return nestColor
-    if (p.isFood) return foodColor
-    // return p.foodPheromone > p.nestPheromone
-    //   ? foodColorMap.scaleColor(p.foodPheromone, 0, 1)
-    //   : nestColorMap.scaleColor(p.nestPheromone, 0, 1)
-  },
-  turtlesShape: 'bug',
-  turtlesSize: 3,
-  turtlesColor: t => (t.carryingFood ? nestColor : foodColor),
-}
+  const drawOptions = {
+    patchesColor: p => {
+      if (p.isNest) return nestColor
+      if (p.isFood) return foodColor
+      // return p.foodPheromone > p.nestPheromone
+      //   ? foodColorMap.scaleColor(p.foodPheromone, 0, 1)
+      //   : nestColorMap.scaleColor(p.nestPheromone, 0, 1)
+    },
+    turtlesShape: 'bug',
+    turtlesSize: 3,
+    turtlesColor: t => (t.carryingFood ? nestColor : foodColor),
+  }
 
 const model = new Model()
 // await model.startup()
@@ -57,14 +59,15 @@ model.setup()
 
 // util.toWindow({ util, model, view, anim })
 
-const TestComponent: React.FC<TestComponentProps> = ({ heading, content }) => (
-  <div data-testid="test-component" className="test-component">
-    <h1 data-testid="test-component__heading" className="heading">
-      {heading}
-    </h1>
-    <div data-testid="test-component__content">{content}</div>
-    <div id="modelDiv"></div>
-  </div>
-);
+  return (
+    <div data-testid="test-component" className="test-component">
+      <h1 data-testid="test-component__heading" className="heading">
+        {heading}
+      </h1>
+      <div data-testid="test-component__content">{content}</div>
+      <div id="modelDiv"></div>
+    </div>
+  )
+};
 
 export default TestComponent;
