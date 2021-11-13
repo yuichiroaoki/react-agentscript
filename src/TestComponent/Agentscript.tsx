3bf587d31e427812cc488e613b6b2b01c0ccd928
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-13 21:20:42 +0900

    show ants example

--- src/TestComponent/Agentscript.tsx
+++ src/TestComponent/Agentscript.tsx
@@ -1,58 +1,62 @@
 import TwoDraw from '../agentscript/TwoDraw.js'
 import Animator from '../agentscript/Animator.js'
-import Color from '../agentscript/Color'
-import ColorMap from '../agentscript/ColorMap'
+// import Color from '../agentscript/Color'
+// import ColorMap from '../agentscript/ColorMap'
 import Model from '../models/AntsModel'
 import React from 'react'
 
 export default function AgentscriptComponent() {
-	// Define colors and colormaps
-	const nestColor = Color.typedColor('yellow')
-	const foodColor = Color.typedColor('blue')
-	const nestColorMap = ColorMap.gradientColorMap(20, [
-		'black',
-		nestColor,
-	])
-	const foodColorMap = ColorMap.gradientColorMap(20, [
-		'black',
-		foodColor,
-	])
 
-	const drawOptions = {
-		patchesColor: p => {
-			if (p.isNest) return nestColor
-			if (p.isFood) return foodColor
-			// return p.foodPheromone > p.nestPheromone
-			//   ? foodColorMap.scaleColor(p.foodPheromone, 0, 1)
-			//   : nestColorMap.scaleColor(p.nestPheromone, 0, 1)
-		},
-		turtlesShape: 'bug',
-		turtlesSize: 3,
-		turtlesColor: t => (t.carryingFood ? nestColor : foodColor),
+	const agentSetup = async () => {
+
+		// Define colors and colormaps
+		// const nestColor = Color.typedColor('yellow')
+		// const foodColor = Color.typedColor('blue')
+		// const nestColorMap = ColorMap.gradientColorMap(20, [
+		// 	'black',
+		// 	nestColor,
+		// ])
+		// const foodColorMap = ColorMap.gradientColorMap(20, [
+		// 	'black',
+		// 	foodColor,
+		// ])
+
+		const drawOptions = {
+			// patchesColor: p => {
+			// 	if (p.isNest) return nestColor
+			// 	if (p.isFood) return foodColor
+			// 	// return p.foodPheromone > p.nestPheromone
+			// 	//   ? foodColorMap.scaleColor(p.foodPheromone, 0, 1)
+			// 	//   : nestColorMap.scaleColor(p.nestPheromone, 0, 1)
+			// },
+			turtlesShape: 'bug',
+			turtlesSize: 3,
+			// turtlesColor: t => (t.carryingFood ? nestColor : foodColor),
+		}
+
+		const model = new Model()
+		await model.startup()
+		model.setup()
+
+		const view = new TwoDraw(model, {
+			div: 'modelDiv',
+			useSprites: true, // ant shape difficult to draw
+			width: 700,
+			drawOptions,
+		})
+
+		const anim = new Animator(
+		  () => {
+		    model.step()
+		    view.draw()
+		  },
+		  50, // run 500 steps
+		  30 // 30 fps
+		)
 	}
+	agentSetup()
 
-	const model = new Model()
-	// await model.startup()
-	model.setup()
-
-	const view = new TwoDraw(model, {
-	  div: 'modelDiv',
-	  useSprites: true, // ant shape difficult to draw
-	  width: 700,
-	  drawOptions,
-	})
-
-	const anim = new Animator(
-	  () => {
-	    model.step()
-	    view.draw()
-	  },
-	  500, // run 500 steps
-	  30 // 30 fps
+	return (
+		<div id="modelDiv"></div>
 	)
-
-
-	const { id, className, style } = this.props;
-
-	return React.createElement('modelDiv');
 }
