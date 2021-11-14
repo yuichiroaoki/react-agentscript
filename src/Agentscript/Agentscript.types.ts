b96ca25b3f0a6a0cfeb4ddc6e091d89d89b79a21
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-14 20:48:42 +0900

    add draw options interface

--- src/Agentscript/Agentscript.types.ts
+++ src/Agentscript/Agentscript.types.ts
@@ -1,6 +1,23 @@
+interface IDrawOptions {
+  patchesColor?: string;
+  //   initPatches?: null;
+  turtlesColor?: string;
+  turtlesStrokeColor?: string;
+  turtlesShape?: string;
+  turtlesSize?: number;
+  linksColor?: string;
+  linksWidth?: number;
+  //   textProperty?: null;
+  textSize?: number;
+  textColor?: string;
+  patchesMap?: string;
+  turtlesMap?: string;
+}
+
 interface IView {
   width: number;
   height: number;
+  drawOptions?: IDrawOptions;
 }
 
 interface IAnimation {
